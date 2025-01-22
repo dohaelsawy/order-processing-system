b941306e44b32bc9efdcea2376ec21c12d12ad0f
dohaelsawy <dohaelsawy18@gmail.com>
2025-01-22 06:05:45 +0200

    feat: add config class and collection names

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_login import LoginManager
+from config import Config
+from flask_pymongo import PyMongo
+from flask_mail import Mail
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
+
+
+login_manager = LoginManager(app)
+mongo = PyMongo(app)
+mail = Mail(app)
+
+
+
+users = mongo.db.users
+inventory = mongo.db.products
+orders = mongo.db.orders
