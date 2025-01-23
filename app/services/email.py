15b3f9a9507e8d0a0f58754afce03a2338398733
dohaelsawy <dohaelsawy18@gmail.com>
2025-01-23 08:23:50 +0200

    refactor: email service

--- app/services/email.py
+++ app/services/email.py
@@ -1,29 +1,30 @@
 from flask_mail import Message
 from app import mail
+from flask import jsonify
 
 
-def send_order_confirmation_email(from_email,to_email, order_id, purchased_items, total_amount, order_date,status,shipping_address,payment_status,payment_method):
-    subject = f"Order Confirmation - Order #{order_id}"
+def send_order_confirmation_email(from_email,to_email, order):
+    subject = f"Order Confirmation - Order #{str(order['order_id'])}"
     body = f"""
     Hello,
 
     Thank you for your order! Here are the details:
 
-    Order ID: {order_id}
-    In {order_date}
-    Which has state {status}
-    Deliver at {shipping_address}
-    has payment status {payment_status}
-    payment method is {payment_method}
+    Order ID: {str(order['order_id'])}
+    In {order['order_date']}
+    Which has state {order['status']}
+    Deliver at {order['shipping_address']}
+    has payment status {order['payment_status']}
+    payment method is {order['payment_method']}
     Purchased Items:
     """
     
-    for item in purchased_items:
+    for item in order['products']:
         body += f"\n- {item['product_name']} (x{item['quantity']}) - ${item['price'] * item['quantity']:.2f}"
 
     body += f"""
     
-    Total Amount: ${total_amount:.2f}
+    Total Amount: ${order['total_amount']:.2f}
 
     We hope you enjoy your purchase!
 
@@ -31,8 +32,12 @@ def send_order_confirmation_email(from_email,to_email, order_id, purchased_items
     Your Store Team
     """
 
-    msg = Message(subject=subject, sender=from_email, recipients=[to_email])
-    msg.body = body
-    mail.send(msg)
-    return True
+    try :
+        msg = Message(subject=subject, sender=from_email, recipients=[to_email])
+        msg.body = body
+        mail.send(msg)
+        return jsonify({"message":"email send successfully"})
+    except Exception as e:
+        return jsonify({"message":f"there is something wrong happens {e}"})
+
     
